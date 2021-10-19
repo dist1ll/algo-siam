1c3471caf5cbbf64bc69b2dff6baa341c7d96c61
m2q <alic.adrian.berlin@gmail.com>
2021-10-19 02:01:15 +0200

    Add basic client functions to AlgorandClientWrapper

--- core/client/wrapper.go
+++ core/client/wrapper.go
@@ -1,6 +1,30 @@
 package core
 
+import (
+	"context"
+
+	"github.com/algorand/go-algorand-sdk/client/v2/algod"
+	"github.com/algorand/go-algorand-sdk/client/v2/common/models"
+)
+
 // AlgorandClientWrapper implements the AlgorandClient interface by wrapping the original
 // algod.Client
-type AlgorandClientWrapper struct{}
+type AlgorandClientWrapper struct {
+	Client algod.Client
+}
 
+func (a *AlgorandClientWrapper) HealthCheck(ctx context.Context) error {
+	return a.Client.HealthCheck().Do(ctx)
+}
+
+func (a *AlgorandClientWrapper) Status(ctx context.Context) (models.NodeStatus, error) {
+	return a.Client.Status().Do(ctx)
+}
+
+func (a *AlgorandClientWrapper) AccountInformation(s string, ctx context.Context) (models.Account, error) {
+	return a.Client.AccountInformation(s).Do(ctx)
+}
+
+func (a *AlgorandClientWrapper) GetApplicationByID(id uint64, ctx context.Context) (models.Application, error) {
+	return a.Client.GetApplicationByID(id).Do(ctx)
+}
