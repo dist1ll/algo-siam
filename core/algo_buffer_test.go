271dd0a373c639bad1c274da5e27dd11f2c9c14b
m2q <alic.adrian.berlin@gmail.com>
2021-10-20 19:56:17 +0200

    Add stretchr/testify package and assertions to tests

--- core/algo_buffer_test.go
+++ core/algo_buffer_test.go
@@ -2,33 +2,53 @@ package core
 
 import (
 	"errors"
-	"github.com/m2q/aema/core/client"
+	core "github.com/m2q/aema/core/client"
 	"testing"
-	//"github.com/stretchr/testify"
+
+	"github.com/algorand/go-algorand-sdk/client/v2/common/models"
+	"github.com/stretchr/testify/assert"
 )
 
-
-// If the HealthCheck is not working, return error upon buffer creation
-func TestAlgorandBuffer_NoHealth(t *testing.T) {
+// If HealthCheck and token verification works, expect no errors
+func TestAlgorandBuffer_HealthAndTokenPass(t *testing.T) {
 	client := core.CreateAlgorandClientMock("", "")
-	client.SetError(false, (*core.AlgorandMock).HealthCheck)
-
 	_, err := NewAlgorandBuffer(client, GeneratePrivateKey64())
 	if err != nil {
 		t.Errorf("failing health check doesn't return error %s", err)
 	}
-
-	// buffer should still have created account
-
 }
 
+// If the HealthCheck is not working, return error upon buffer creation
+func TestAlgorandBuffer_NoHealth(t *testing.T) {
+	client := core.CreateAlgorandClientMock("", "")
+	client.SetError(true, (*core.AlgorandMock).HealthCheck)
+	buffer, err := NewAlgorandBuffer(client, GeneratePrivateKey64())
+	if err == nil {
+		t.Errorf("failing health check doesn't return error %s", err)
+	}
+	// buffer should still have created account
+	assert.NotEqual(t, models.Account{}, buffer.Account)
+}
+
+// If the Token Verification is not working, return error upon buffer creation
+func TestAlgorandBuffer_IncorrectToken(t *testing.T) {
+	client := core.CreateAlgorandClientMock("", "")
+	client.SetError(true, (*core.AlgorandMock).Status)
+	buffer, err := NewAlgorandBuffer(client, GeneratePrivateKey64())
+	if err == nil {
+		t.Errorf("failing token verification doesn't return error %s", err)
+	}
+	// buffer should still have created account
+	assert.NotEqual(t, models.Account{}, buffer.Account)
+}
 
 func TestChainAppCreationDeletion(t *testing.T) {
+	return
 	a, err := NewAlgorandBufferFromEnv()
 
 	//fmt.Println(runtime.FuncForPC(reflect.ValueOf(a.CreateApplication).Pointer()).Name())
 	//a.CreateApplication()
-	return
+
 	if e := &(NoApplication{}); errors.As(err, &e) {
 		t.Logf("no apps registered under %s", e.Account.Address)
 		err = a.CreateApplication()
--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/PuerkitoBio/goquery v1.7.1
 	github.com/algorand/go-algorand-sdk v1.11.0
 	github.com/google/go-querystring v1.1.0 // indirect
+	github.com/stretchr/testify v1.7.0
 	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519 // indirect
 	golang.org/x/sys v0.0.0-20211013075003-97ac67df715c // indirect
 )
