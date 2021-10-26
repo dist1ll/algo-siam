853cd109bd0aa7ae4659a8e75de1aaa5f4c73c05
m2q <alic.adrian.berlin@gmail.com>
2021-10-26 21:56:46 +0200

    Add validity integration test and build flags

--- core/integration_test.go
+++ core/integration_test.go
@@ -1,3 +1,21 @@
 // +build integration
 
 package core
+
+import (
+	"context"
+	"github.com/m2q/aema/core/client"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestIntegration_StoreData(t *testing.T) {
+	buffer, _ := CreateAlgorandBufferFromEnv()
+
+	ctx, cancel := context.WithTimeout(context.Background(), client.AlgorandDefaultTimeout)
+	info, err := buffer.Client.AccountInformation(buffer.AccountCrypt.Address.String(), ctx)
+	cancel()
+
+	assert.Nil(t, err)
+	assert.Equal(t, 1, len(info.CreatedApps))
+}
