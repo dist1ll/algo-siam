886fc8f08324cbbdcabba7311417eaf915d43d51
m2q <alic.adrian.berlin@gmail.com>
2021-10-27 22:28:20 +0200

    Test integration for application creation in Manage()

--- core/integration_test.go
+++ core/integration_test.go
@@ -4,9 +4,12 @@ package core
 
 import (
 	"context"
+	"github.com/algorand/go-algorand-sdk/client/v2/common/models"
 	"github.com/m2q/aema/core/client"
 	"github.com/stretchr/testify/assert"
+	"sync"
 	"testing"
+	"time"
 )
 
 // Note: During integration tests, we need to make sure that there's no
@@ -24,7 +27,9 @@ func TestIntegration_ValidAccount(t *testing.T) {
 	assert.Equal(t, 1, len(info.CreatedApps))
 }
 
-func TestIntegration_RemoveAccount(t *testing.T) {
+// Utility function that creates an AlgorandBuffer, and subsequently deletes the application
+// so that only one remains.
+func createBufferAndRemoveApps(t *testing.T) *AlgorandBuffer {
 	buffer, err := CreateAlgorandBufferFromEnv()
 	if err != nil {
 		t.Fatal(err)
@@ -39,8 +44,34 @@ func TestIntegration_RemoveAccount(t *testing.T) {
 	info, err := buffer.Client.AccountInformation(buffer.AccountCrypt.Address.String(), context.Background())
 	assert.Nil(t, err)
 	assert.Equal(t, 0, len(info.CreatedApps))
+
+	return buffer
+}
+
+func TestIntegration_RemoveAccount(t *testing.T) {
+	_ = createBufferAndRemoveApps(t)
 }
 
 func TestIntegration_AccountGetsRestored(t *testing.T) {
+	buffer := createBufferAndRemoveApps(t)
 
+	var wg sync.WaitGroup
+	ctx, cancel := context.WithCancel(context.Background())
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		buffer.Manage(ctx, &ManageConfig{})
+	}()
+
+	var info models.Account
+	for !client.ValidAccount(info) {
+		info, _ = buffer.Client.AccountInformation(buffer.AccountCrypt.Address.String(), ctx)
+		time.Sleep(time.Second)
+	}
+
+	cancel()
+
+	if waitTimeout(&wg, time.Second) {
+		t.Fatalf("goroutine didn't finish in time")
+	}
 }
